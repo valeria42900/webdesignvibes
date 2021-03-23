a6fd1a5963e2d644d246b11e0c6a47015b4c875a
valeria42900 <78056084+valeria42900@users.noreply.github.com>
2021-03-22 23:12:42 -0400

    workshops

---
+++ workshops/java-exercise/functions.js
@@ -0,0 +1,47 @@
+/**
+ * Example of starting a plugin with options.
+ * I am just passing some of the options in the following example.
+ * you can also start the plugin using $('.marquee').marquee(); with defaults
+*/
+$('.marquee').marquee({
+	//duration in milliseconds of the marquee
+	duration: 15000,
+	//gap in pixels between the tickers
+	gap: 50,
+	//time in milliseconds before the marquee will start animating
+	delayBeforeStart: 0,
+	//'left' or 'right'
+	direction: 'left',
+	//true or false - should the marquee be duplicated to show an effect of continues flow
+	duplicated: true,
+  pauseOnHover: true,
+  startVisible: true,
+});
+
+$( function() {
+    $( ".draggable" ).draggable();
+  } );
+
+
+  // external js: flickity.pkgd.js
+
+  var $carousel = $('.carousel').flickity({
+    imagesLoaded: true,
+    percentPosition: false,
+  });
+
+  var $imgs = $carousel.find('.carousel-cell img');
+  // get transform property
+  var docStyle = document.documentElement.style;
+  var transformProp = typeof docStyle.transform == 'string' ?
+    'transform' : 'WebkitTransform';
+  // get Flickity instance
+  var flkty = $carousel.data('flickity');
+
+  $carousel.on( 'scroll.flickity', function() {
+    flkty.slides.forEach( function( slide, i ) {
+      var img = $imgs[i];
+      var x = ( slide.target + flkty.x ) * -1/3;
+      img.style[ transformProp ] = 'translateX(' + x  + 'px)';
+    });
+  });
