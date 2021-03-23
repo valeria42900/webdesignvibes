fac761d77bf18cd940d9723cf68d7af004e3a123
valeria42900 <78056084+valeria42900@users.noreply.github.com>
2021-03-22 21:39:55 -0400

    added homepage

---
+++ projects/homepage/functions.js
@@ -0,0 +1,33 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+document.getElementById("showImage").onclick = function() {
+    document.getElementById("theImage").style.visibility = "visible";
+}
+
+
+<script>
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+</script>
